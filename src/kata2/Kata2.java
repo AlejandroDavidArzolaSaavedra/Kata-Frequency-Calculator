8b5b2a1f1076b884c9ad00bae6499170af314be2
AlejandroDavidArzolaSaavedra <alejandro.arzola101@alu.ulpgc.es>
2021-10-15 19:20:07 +0100

    kata2 version1

---
+++ src/kata2/Kata2.java
@@ -0,0 +1,29 @@
+package kata2;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Kata2 {
+
+    public static void main(String[] args) {
+        
+        int [] data = { 1 , 4 , 1 , 100 , 2 , -4 , 100 ,
+                       100 , 1 , 2 , 6 , 100 ,  8 , 4 , 
+                       4 , 4 , 100 , 4 , 5 , 5 , 5 ,
+                       5 , 5 , 5 , 6 , 100 , 6 , 6 ,
+                       100 , 6 , 6 , 6 , 6 , 8 , 6 };
+        
+        Map<Integer, Integer> histogram = new HashMap<Integer, Integer>();
+            for (int i = 0; i < data.length; i++) {
+                if (histogram.containsKey(data[i])) {
+                    histogram.put(data[i], histogram.get(data[i])+1); 
+                }else{
+                    histogram.put(data[i], 1);
+                }
+            }
+        for (Integer key : histogram.keySet()) {
+            System.out.println(key + "==>" + histogram.get(key));
+            }
+        }
+
+    }
