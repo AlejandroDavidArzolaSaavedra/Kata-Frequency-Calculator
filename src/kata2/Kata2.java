6a6550d6c919a24ba06e9715ddc12413dbe1620d
AlejandroDavidArzolaSaavedra <alejandro.arzola101@alu.ulpgc.es>
2021-10-16 14:55:08 +0100

    kata2 version5

--- src/kata2/Kata2.java
+++ src/kata2/Kata2.java
@@ -7,14 +7,10 @@ public class Kata2 {
 
     public static void main(String[] args) {
         
-        Integer [] data = { 1 , 4 , 1 , 100 , 2 , -4 , 100 ,
-                       100 , 1 , 2 , 6 , 100 ,  8 , 4 , 
-                       4 , 4 , 100 , 4 , 5 , 5 , 5 ,
-                       5 , 5 , 5 , 6 , 100 , 6 , 6 ,
-                       100 , 6 , 6 , 6 , 6 , 8 , 6 };
+        String [] data = {"Rosa", "Pepe", "María", "Pepe", "Pepe", "Rosa"};
         Histogram histo = new Histogram(data);
-        Map<Integer,Integer> histogr = histo.getHistogram();
-        for (Integer key : histogr.keySet()) {
+        Map<String,Integer> histogr = histo.getHistogram();
+        for (String key : histogr.keySet()) {
             System.out.println(key + "==>" + histogr.get(key));
             }
         }
